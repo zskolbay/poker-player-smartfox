6c7da8d26dbac7881f89fc422e01730c09f7db2a
Zsolt Kolbay <Zsolt@ifs.aero>
2016-04-09 12:55:26 +0200

    sdcsadcsacsdac

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -26,7 +26,12 @@ namespace Nancy.Simple
             try
             {
                 Logger.LogHelper.Log("type=bet_begin action=bet_request request_id={0} game_id={1}", requestId, gameState.GameId);
-                if (false)//(gameState.CommunityCards.Count() <= 2)
+
+                if (gameState.GetCurrentPlayer().Stack < 100)
+                {
+                    bet = 0;
+                }
+                else if (false)//(gameState.CommunityCards.Count() <= 2)
                 {
                     // Pre Flop
                     Logger.LogHelper.Log("type=Pre Flop block action=bet_request request_id={0} game_id={1}", requestId, gameState.GameId);
