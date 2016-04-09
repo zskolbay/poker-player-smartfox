f1bc54800505cec743c4c3c6572799459af81622
Zsolt Kolbay <Zsolt@ifs.aero>
2016-04-09 16:28:01 +0200

    ddddd

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -172,7 +172,7 @@ namespace Nancy.Simple
                     }
                     else
                     {
-                        if (new Random().Next() % 10 == 0)
+                        if (new Random().Next() % 5 == 0)
                         {
                             bet = gameState.CurrentBuyIn + Math.Max(gameState.MinimumRaise, 50) -
                                   gameState.GetCurrentPlayer().Bet;
