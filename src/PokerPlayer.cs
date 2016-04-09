6ef54f0bb9be1fe13570a5a92de43e6a0666d0d8
Tibor Kanyó <kanyo.tibi@gmail.com>
2016-04-09 10:05:45 +0200

    try catch around bet request

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,15 +1,38 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
+        private static Guid requestId = Guid.NewGuid();
+        public static Guid RequestId { get { return requestId; } }
+        public static Guid GenerateRequestId()
+        {
+            return (requestId = Guid.NewGuid());
+        }
+
+
 		public static readonly string VERSION = "Default C# folding player";
 
-		public static int BetRequest(JObject gameState)
+		public static int BetRequest(JObject jsonState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+		    int bet = 0;
+            var gameState = JsonConvert.DeserializeObject<GameState>(jsonState.ToString());
+
+            try
+            {
+                //TODO: Use this method to return the value You want to bet
+                Logger.LogHelper.Log("type=bet_begin action=bet_request request_id={0} game_id={1}", requestId, gameState.GameId);
+               
+            }
+            catch (Exception ex)
+            {
+                Logger.LogHelper.Error("type=error action=bet_request request_id={0} game_id={1} error_message={2}", requestId, gameState.GameId, ex);
+            }
+
+            return bet;
 		}
 
 		public static void ShowDown(JObject gameState)
